84b69fd06f96c2741f7118a45746f6bf6e878083
Demmarl <demmarlmiau@gmail.com>
2024-05-13 14:49:50 -0600

    nombres de los integrantes

--- EjemploPracticoATM.cpp
+++ EjemploPracticoATM.cpp
@@ -5,6 +5,18 @@
 // la función main crea y ejecuta el ATM
 int main()
 {
+
+ /* - PROYECTO FINAL
+ - ATM
+ - EQUIPO #4
+ - NOMBRES DE LOS INTEGRANTES
+    Rivera Lopez Diego Emmanuel
+    Rocha Murillo Jonathan Joshua
+    Ramirez Molina Asael Alejandro
+    Quintero Carreón Jorge Isaac
+ */
+
+
 ATM atm; // crea un objeto ATM
 atm.ejecutar(); // indica al ATM que debe empezar
 return 0;
